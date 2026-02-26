e7f0f39cce65fdf8f6aa67204c214a409fdd5c9f
asovi <y.sugita.asovi@gmail.com>
2026-02-26 15:55:25 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT2WSZGibHfo4AHqFYWbQHpLqqrCM-181WQpJx22zjPFKr9UzGRPd4fZhtnE4lTTPZ_WsIm7xJpj8wG/pub?output=csv";
+
+fetch(sheetURL)
+  .then(res => res.text())
+  .then(data => {
+    const rows = data.split("\n").slice(1);
+    const container = document.getElementById("projects");
+
+    rows.forEach(row => {
+      const cols = row.split(",");
+      if(cols.length < 7) return;
+
+      const card = document.createElement("div");
+      card.className = "card";
+
+      card.innerHTML = `
+        <h2>${cols[0]}</h2>
+        <div class="meta">
+          <span class="badge">単価：${cols[1]}</span>
+          <span class="badge">経験：${cols[2]}</span><br>
+          エリア：${cols[3]}<br>
+          稼働日数：${cols[4]}<br>
+          担当：${cols[6]}
+        </div>
+        <div>
+          <strong>業務内容</strong><br>
+          ${cols[5]}
+        </div>
+      `;
+
+      container.appendChild(card);
+    });
+  });
+
+document.getElementById("search").addEventListener("input", function(){
+  const keyword = this.value.toLowerCase();
+  document.querySelectorAll(".card").forEach(card=>{
+    card.style.display =
+      card.innerText.toLowerCase().includes(keyword)
+        ? "block"
+        : "none";
+  });
+});
