85bc9efc0322a6eff13dada4488b566f059f2a89
asovi <y.sugita.asovi@gmail.com>
2026-05-25 18:30:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT2WSZGibHfo4AHqFYWbQHpLqqrCM-181WQpJx22zjPFKr9UzGRPd4fZhtnE4lTTPZ_WsIm7xJpj8wG/pubhtml?gid=0&single=true";
+const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT2WSZGibHfo4AHqFYWbQHpLqqrCM-181WQpJx22zjPFKr9UzGRPd4fZhtnE4lTTPZ_WsIm7xJpj8wG/pub?output=csv";
 
 // =====================
 // CSVパーサー
